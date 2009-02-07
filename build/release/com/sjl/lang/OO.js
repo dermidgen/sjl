7e007e8ab6126f68ad8b2931941cd32a948ed00f
good.midget <good.midget@058e897e-f256-11dd-9d1d-173cc1d06e3c>
2009-02-07 00:05:14 +0000

    Added task to ant build for adding license block to distribution files
    git-svn-id: https://sjl.googlecode.com/svn/trunk@5 058e897e-f256-11dd-9d1d-173cc1d06e3c

--- build/release/com/sjl/lang/OO.js
+++ build/release/com/sjl/lang/OO.js
@@ -1,3 +1,14 @@
+/**
+ * Standard JavaScript Library
+ *
+ * LICENSE
+ *
+ * This source file is subject to the sjl Public License license that
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @copyright Copyright (c) 2008, Danny Graham, Scott Thundercloud
+ */
 var Class={create:function(_1){
 var _2=function(){
 if(this.__construct){
--- test/com/sjl/io/Http.js
+++ test/com/sjl/io/Http.js
@@ -1,3 +1,14 @@
+/**
+ * Standard JavaScript Library
+ *
+ * LICENSE
+ *
+ * This source file is subject to the sjl Public License license that
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @copyright Copyright (c) 2008, Danny Graham, Scott Thundercloud
+ */
 Import("com.sjl.EventDispatcher");
 Import("com.sjl.io.XMLHttpRequest");
 Namespace("com.sjl.io");
